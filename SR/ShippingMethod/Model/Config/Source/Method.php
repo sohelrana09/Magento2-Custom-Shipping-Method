72d8b0695a23a0f7ac7d56b7ee2cc2936eea91ac
Md. Sohel Rana <sohelrana09@gmail.com>
2015-05-21 12:26:41 +0600

    Initial commit

---
+++ SR/ShippingMethod/Model/Config/Source/Method.php
@@ -0,0 +1,17 @@
+<?php
+namespace SR\ShippingMethod\Model\Config\Source;
+
+class Method implements \Magento\Framework\Option\ArrayInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function toOptionArray()
+    {
+        return [
+            ['value' => '', 'label' => __('None')],
+            ['value' => 'O', 'label' => __('Per Order')],
+            ['value' => 'I', 'label' => __('Per Item')]
+        ];
+    }
+}
